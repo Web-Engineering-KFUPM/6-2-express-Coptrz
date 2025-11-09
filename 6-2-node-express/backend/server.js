da70f5931c568357c8e06dc1a0499f4905ef56bf
Coptrz <nwaclos@gmail.com>
2025-11-09 12:02:59 +0300

    done

--- 6-2-node-express/backend/server.js
+++ 6-2-node-express/backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import { getRandomQuote } from "./quotes.js";
 const app = express();
-const Port = 3000;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
@@ -10,13 +10,12 @@ app.listen(PORT, () => {
 
 app.use(cors());
 app.get("/", (req, res) => {
-  res.send("Welcome to the Quotes API!");
+  res.send("Welcome to the Quote Generator API");
 });
-
 
 app.get("/api/quote", (req, res) => {
   const quote = getRandomQuote();
-  res.json(quote);
+  res.json({ quote });
 });
 
 
